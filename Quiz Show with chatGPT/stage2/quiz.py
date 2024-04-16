0ef6046a0c17378d3981b0f1619635093c521106
MajewskiL <lukasz.majewski@tlen.pl>
2024-04-16 22:00:15 +0200

    Solution dor stage 2

--- Quiz Show with chatGPT/stage2/quiz.py
+++ Quiz Show with chatGPT/stage2/quiz.py
@@ -1,1 +1,29 @@
-print('Hello World!')
+from openai import OpenAI
+import os
+
+API_KEY = os.environ.get('API_KEY')
+
+
+def ask_chat(q):
+    client = OpenAI(api_key=API_KEY)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        temperature=0.5,
+        messages=[
+            {
+                "role": "user",
+                "content": f"{q}"
+            }
+        ],
+    )
+    answer = response.choices[0].message.content
+    return answer
+
+
+topic = input("Enter the quiz topic: ")
+
+#  Stage 2 - q1: python os library
+mark = "TaskMark:"
+chat_question_1 = (f"Print four short facts about {topic} line by line, without bullet points, and each fact should have prefix '{mark}'.")
+chat_answer_1 = ask_chat(chat_question_1)
+print(chat_answer_1)
