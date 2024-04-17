24730a9807ac1752acae6685010f21cf82c053b9
MajewskiL <lukasz.majewski@tlen.pl>
2024-04-17 20:31:28 +0200

    Added idea of stage

--- Quiz Show with chatGPT/stage4/quiz.py
+++ Quiz Show with chatGPT/stage4/quiz.py
@@ -1,1 +1,6 @@
-print('Hello World!')
+
+
+# Stage 4 - q1: python os library
+# pytamy tak długa jak nie exit lub nie udzielono poprwnych odpowiedzi. Każda poprawna usuwa pytanie.
+# Każda zła zamienia pytanie i nie mogą się powtarzać - do trzech. - najlepiej by było losowo true/false
+# ale na potrzeby testów robimy oposite.
